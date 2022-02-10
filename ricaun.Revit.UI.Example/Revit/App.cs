119079312fdebe4707d1adee217f08170d76a6bc
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-02-10 11:10:43 -0300

    Test Binding

--- ricaun.Revit.UI.Example/Revit/App.cs
+++ ricaun.Revit.UI.Example/Revit/App.cs
@@ -5,6 +5,7 @@ using System;
 using System.Linq;
 using ricaun.Revit.UI.Example.Proprieties;
 using System.Windows.Media;
+using System.Collections.Generic;
 
 namespace ricaun.Revit.UI.Example.Revit
 {
@@ -358,13 +359,23 @@ namespace ricaun.Revit.UI.Example.Revit
 
             //c.SetImage(Proprieties.Icons8.Document.GetBitmapFrame());
             //c.GetRibbonItem().Width = 32;
-            Console.WriteLine(c.GetRibbonItem().Width);
+            //Console.WriteLine(c.GetRibbonItem().Width);
 
             var rvtRibbonCombo = c.GetRibbonItem() as UIFramework.RvtRibbonCombo;
 
             rvtRibbonCombo.IsTextSearchEnabled = true;
+            var binding = new System.Windows.Data.Binding();
+            //binding.Source = new List<TextModel>() { new TextModel("Test") };
+            binding.Source = new List<Autodesk.Windows.RibbonItem>() { new Autodesk.Windows.RibbonItem() {
+                Text = "hi", Image = Proprieties.Icons8.Document.GetBitmapFrame() } };
+            //binding.Path = "";
+            rvtRibbonCombo.ItemsBinding = binding;
             //rvtRibbonCombo.IsEditable = true;
-
+            rvtRibbonCombo.CurrentChanged += (s, e) =>
+            {
+                Console.WriteLine(e.NewValue);
+                Console.WriteLine(e.OldValue);
+            };
 
             return c;
 
@@ -400,6 +411,15 @@ namespace ricaun.Revit.UI.Example.Revit
         {
             const string LargeImage = "iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAACQAAAAkABgV+WigAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAPaSURBVFiFvZddbBRVGIaf+dnd2bp/dQu0paStho2kQVpNL0RDQ/WiCVZEozHGLImpvS0iBIxKkTRGEU29gVTlRgJBuRQ1xliLEGmDSa1EbWJtKy3VdmmVsmt3Z3/Gi+ngst2fmSHxTc7Fzvm+931nvzPnfEfAPHzANqAV2ATUAYHlub+BSeAHoB/4DLhhgbsoQsBxIAZoJkcM+BBYfzvCbuCI4FCSFoRzhwocBhSr4uuBy4BW0dGnBcPvaWKZ364JDbgIVJkVbwLmjOSKzuNa/SlNq33/muZr69IQJbsmpoB7c8XEPG/+JbBqRaAnSDDcy9qeSyj3bDH7MtmoQV+clYUMKMCZfOLZcNY1UXXgHGv2foq8qt6OibPo6wsAKWvyTWBHbkbZ/dtx1TWtYHJUhfA93Ino9pIYG0JLqWZNVANpYAD++wdCQJdZBgOC042/fR8174zibe0EIbeiBbGb5VIYGfsA2aoBA1J5NRUdfVQfGkQJbTaT4gEOGAZ8wDN2xbPhuruZqu4LrO76BDm4rlR4GPBKwBPFDDjXbcTd0GrehSDgrGnA29oBmQzqxPeQSeelBkYk9NrfV4gvPnqe+M8DuOoakQKVhcJW+pBduDc+gmfLTjLRedQrI/nCFiTgNWBtMbLUtd+50f8BqblxlNBmRMVj2ohY5ueO5h24N7SgTg6Tvj6bPZ0UgAhQYZpQ8eDftgf/Y/sRHC7TRgDQMkQvnGTh5B7Si3MAcxLQw637QXGOlEr8lwFigx8jBSpx1jSYNyAIOGs34d3aAYA6fkk2/eHm5XO6SweVgAwsYqUEZQEC2/fja9t1GyV4ifRiBOC6DEyYMiCIeB56jjufPYzkX2NNGFj6qZ+FEy+iXvkx+/G4jN5GNRdLVja0EAz34qxttCycnP2Nv06/TGzoTL7pERm9h3uhEEHg8Vcof7rHsjDA0uWvmD3SjpZMFAr5WkQ/HmOFIuTVd9kSB0jNTxUTjwFfiEAUOG1bxT5OAVHjM3wLSP6P4ip6/3HzOP4V6LXKoqlLaOqSHQPvAuPZBkA/nwfNMvwzfJbpvQ1M7w4RPX8CNM1s6kXgoPEj20AcvSWbKpatTg7zx6EWZt9uJxWZILUwTeRYmJnuB0iMlfQ/AzwF3FyZuVvxn8CjwHRuZiY6z/xHu7j6ajPx0W9XMCfGhpjpfpDIsZ25J56BKaANuFrKJeid8TlsXkxExaOVP3lQCz5/1Hj2HTntuBm4gNcFhxI1K5w7BIeSAN5Y5rKNSuAo+n5hVjwK9AEldzHBghEP+vV8K9AI1HPr9XwCGAa+AT5fNlES/wLKWXKx84783QAAAABJRU5ErkJggg==";
             return LargeImage;
+        }
+    }
+
+    internal class TextModel
+    {
+        public string Text { get; set; }
+        public TextModel(string text)
+        {
+            Text = text;
         }
     }
 }
