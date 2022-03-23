a084daecf205ab919c384bd25ed8202e5d3d0492
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-03-23 14:53:59 -0300

    Version 0.0.9 - Add CreateOrSelectPanel

--- Build/Build.cs
+++ Build/Build.cs
@@ -6,9 +6,7 @@ using ricaun.Nuke.Components;
 [CheckBuildProjectConfigurations]
 class Build : NukeBuild, IPublishPack, IRevitPackageBuilder
 {
-    //string IHazPackageBuilderProject.Name => this.From<IHazExample>().Name;
     string IHazPackageBuilderProject.Name => "Example";
     string IHazRevitPackageBuilder.Application => "Revit.App";
-    //bool IHazExample.ReleaseExample => false;
     public static int Main() => Execute<Build>(x => x.From<IPublishPack>().Build);
 }
