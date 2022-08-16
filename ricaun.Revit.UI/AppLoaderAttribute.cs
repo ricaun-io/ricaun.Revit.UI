413d5f716372f17f3c8d10388dca1a06ae117b89
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-08-16 19:49:41 -0300

    Version 0.3.2 ### Features - Add `AppLoader` Attribute

---
+++ ricaun.Revit.UI/AppLoaderAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ricaun.Revit.UI
+{
+    /// <summary>
+    /// Console Attribute for ricaun.AppLoader
+    /// </summary>
+    public class ConsoleAttribute : Attribute
+    {
+    }
+
+    /// <summary>
+    /// AppLoader Attribute for ricaun.AppLoader
+    /// </summary>
+    public class AppLoaderAttribute : Attribute
+    {
+    }
+
+    /// <summary>
+    /// ApplicationLoader Attribute for ricaun.AppLoader
+    /// </summary>
+    public class ApplicationLoaderAttribute : Attribute
+    {
+    }
+}
