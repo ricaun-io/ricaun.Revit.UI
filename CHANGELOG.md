1f9f1291f5705828aaf321f45b9c318a3b759115
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2023-11-03 18:23:29 -0300

    Language Update and Test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [0.5.5] / 2023-11-03
+### Features
+- Language Update and Test
+
 ## [0.5.4] / 2023-10-19
 ### Features
 - Add `ComboBox` methods extension
@@ -275,6 +279,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - First Release
 
 [vNext]: ../../compare/1.0.0...HEAD
+[0.5.5]: ../../compare/0.5.4...0.5.5
 [0.5.4]: ../../compare/0.5.3...0.5.4
 [0.5.3]: ../../compare/0.5.2...0.5.3
 [0.5.2]: ../../compare/0.5.1...0.5.2
--- ricaun.Revit.UI.Tests/RevitTests.cs
+++ ricaun.Revit.UI.Tests/RevitTests.cs
@@ -1,3 +1,5 @@
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using NUnit.Framework;
 using System;
@@ -20,4 +22,69 @@ namespace ricaun.Revit.UI.Tests
             Console.WriteLine(uiapp.Application.VersionName);
         }
     }
+
+    public class RevitLanguageTests
+    {
+        private Application application;
+        [OneTimeSetUp]
+        public void Setup(Application application)
+        {
+            this.application = application;
+        }
+
+        [Test]
+        public void Application_Language()
+        {
+            var language = application.Language;
+
+            var name = language.GetCultureInfoName();
+            Assert.IsNotNull(name);
+        }
+
+        [Test]
+        public void Languages_Valid_CultureInfoName()
+        {
+            var languages = new[]
+            {
+                LanguageType.English_USA,
+                LanguageType.French,
+                LanguageType.German,
+                LanguageType.Italian,
+                LanguageType.Japanese,
+                LanguageType.Korean,
+                LanguageType.Polish,
+                LanguageType.Spanish,
+                LanguageType.Chinese_Simplified,
+                LanguageType.Chinese_Traditional,
+                LanguageType.Brazilian_Portuguese,
+                LanguageType.Russian,
+                LanguageType.Czech,
+            };
+
+            foreach (var language in languages)
+            {
+                var name = language.GetCultureInfoName();
+                Assert.IsNotNull(name);
+            }
+        }
+
+        [Test]
+        public void Languages_NotValid_CultureInfoName()
+        {
+            var languages = new[]
+            {
+                LanguageType.Unknown,
+                LanguageType.English_GB,    // Does not exist in Revit Api 2017
+                LanguageType.Dutch,         // Not supported
+                LanguageType.Hungarian,     // Not supported
+            };
+
+            foreach (var language in languages)
+            {
+                var name = language.GetCultureInfoName();
+                Assert.IsNull(name);
+            }
+        }
+    }
+
 }
