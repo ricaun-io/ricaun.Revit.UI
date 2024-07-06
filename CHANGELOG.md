66ff76bd154f5a89db4a5223e5ecdca0a45fc10e
Luiz Henrique Cassettari <ricaun@gmail.com>
2024-07-06 11:10:30 -0300

    Use `GetRibbonItem_Alternative` by default

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [0.6.3] / 2024-07-06
+### Features
+### Updated
+- Change `GetRibbonItem` to `GetRibbonItem_Alternative` to fix null when panel is removed.
+
 ## [0.6.2] / 2024-01-09 - 2024-02-05
 ### Features
 - `SetListImageSize` to change the size of the image in the `PulldownButton` and `SplitButton`.
--- ricaun.Revit.UI.Tests/Extensions/RevitRibbonItemTests.cs
+++ ricaun.Revit.UI.Tests/Extensions/RevitRibbonItemTests.cs
@@ -21,14 +21,51 @@ namespace ricaun.Revit.UI.Tests.Extensions
         }
 
         [Test]
-        public void GetRibbonItem_EqualTo_Alternative()
+        public void GetRibbonItem_RibbonControl_NotNull()
+        {
+            var pushButton = ribbonPanel.CreatePushButton<BaseCommand>();
+            var ribbonItemRibbonControl = pushButton.GetRibbonItem_RibbonControl();
+            Assert.IsNotNull(ribbonItemRibbonControl);
+        }
+
+        [Test]
+        public void GetRibbonItem_RemovePanel()
+        {
+            var pushButton = ribbonPanel.CreatePushButton<BaseCommand>();
+            ribbonPanel.Remove();
+            var ribbonItem = pushButton.GetRibbonItem();
+            Assert.IsNotNull(ribbonItem);
+        }
+
+        [Test]
+        public void GetRibbonItem_Alternative_RemovePanel()
+        {
+            var pushButton = ribbonPanel.CreatePushButton<BaseCommand>();
+            ribbonPanel.Remove();
+            var ribbonItemAlternative = pushButton.GetRibbonItem_Alternative();
+            Assert.IsNotNull(ribbonItemAlternative);
+        }
+
+        [Test]
+        public void GetRibbonItem_RibbonControl_RemovePanel()
+        {
+            var pushButton = ribbonPanel.CreatePushButton<BaseCommand>();
+            ribbonPanel.Remove();
+            var ribbonItemRibbonControl = pushButton.GetRibbonItem_RibbonControl();
+            Assert.IsNull(ribbonItemRibbonControl);
+        }
+
+
+        [Test]
+        public void GetRibbonItem_EqualTo_Alternative_And_RibbonControl()
         {
             var pushButton = ribbonPanel.CreatePushButton<BaseCommand>();
             var ribbonItem = pushButton.GetRibbonItem();
             var ribbonItemAlternative = pushButton.GetRibbonItem_Alternative();
+            var ribbonItemRibbonControl = pushButton.GetRibbonItem_RibbonControl();
             Assert.AreEqual(ribbonItem, ribbonItemAlternative);
+            Assert.AreEqual(ribbonItemRibbonControl, ribbonItemAlternative);
         }
-
     }
 
 }
