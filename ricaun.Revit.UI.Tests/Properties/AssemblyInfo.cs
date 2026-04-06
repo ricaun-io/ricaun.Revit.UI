43cb6d2beba7a6994116c468693809bab49b11d2
ricaun <ricaun@gmail.com>
2026-04-06 14:23:25 -0300

    Update tests

---
+++ ricaun.Revit.UI.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+#if NET && DEBUG
+[assembly: AssemblyMetadata("NUnit.Application", "RICAUN_REVITTEST_TESTADAPTER_NUNIT_APPLICATION_CLIENT_LOCAL")]
+[assembly: AssemblyMetadata("NUnit.Verbosity", "2")]
+#endif
